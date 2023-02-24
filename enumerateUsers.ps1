267c5e2300cf251dab33c39d875c5682332a9b45
oblowngo <126069962+oblowngo@users.noreply.github.com>
2023-02-24 13:37:38 +0100

    Add files via upload

--- enumerateUsers.ps1
+++ enumerateUsers.ps1
@@ -1,7 +1,19 @@
-Param([Parameter(Mandatory=$true,ValueFromPipeline=$true,Position=0)]$domain
-,[Parameter(Mandatory=$true,ValueFromPipeline=$true,Position=1)]$dictionary,
-[Parameter(Mandatory=$true,ValueFromPipeline=$true,Position=2)]$team,
-[Parameter(Mandatory=$true,ValueFromPipeline=$true,Position=3)]$channel)
+Param([Parameter(Mandatory=$true,ValueFromPipeline=$true,Position=0)][string]$domain
+,[Parameter(Mandatory=$false,ValueFromPipeline=$true,Position=1)][string]$dictionary="dictionary.txt",
+[Parameter(Mandatory=$false,ValueFromPipeline=$true,Position=2)][string]$team="xTapTeamxTap",
+[Parameter(Mandatory=$false,ValueFromPipeline=$true,Position=3)][string]$channel="xTapChannelxTap")
+
+if (-not(Test-Path -Path .\$dictionary -PathType Leaf))
+	{
+		Write-Output "Dictionary file not found"
+		exit
+	}
+$mx=Resolve-DnsName -Name solarpack.es -Type MX | select NameExchange|select NameExchange -ExpandProperty NameExchange
+if (-not($mx -Match ("mail.protection.outlook.com")))
+	{
+		Write-Output "Domain not at o365"
+		exit
+	}
 
 $file_dict = Get-Content .\$dictionary
 
@@ -9,13 +21,15 @@ $file_dict = Get-Content .\$dictionary
 Import-Module MicrosoftTeams
 Import-Module Microsoft.Graph.Teams
 
-
+Write-Output "Creating team/channel"
 Connect-MicrosoftTeams
 Connect-MgGraph -Scopes "ChannelMember.ReadWrite.All"
 
 $group = New-Team -MailNickname $team -displayname $team -Visibility "private"
+Start-Sleep -Seconds 2
 New-TeamChannel -GroupId $group.GroupId -DisplayName $channel -MembershipType "Shared"
 $channelId=Get-TeamChannel -GroupId $group.GroupId -MembershipType Shared | Select Id -ExpandProperty Id
+Write-Output "Check users"
  foreach($line in $file_dict)
 {
 	$params = @{
@@ -33,4 +47,8 @@ $channelId=Get-TeamChannel -GroupId $group.GroupId -MembershipType Shared | Sele
 	}
 	 
 	
- } 
+ }
+Write-Output "Deleting team/channel" 
+ Remove-TeamChannel -GroupId $group.GroupId -DisplayName $channel
+ Start-Sleep -Seconds 2
+ Remove-Team -GroupId $group.GroupId
